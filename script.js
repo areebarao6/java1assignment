5536191dda84eb1d3b45448bdb5a26d730dea349
areeba <143202103+areebarao6@users.noreply.github.com>
2023-10-07 21:51:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//use of console.log//
+console.log("Aslamualikum Miss Iqra");
+// Step 1: Set a variable value
+let a = 10; // Set a value of 10
+
+// Step 2: Print the value using console.log()
+console.log(a); // This will output the value of 'a' to the console
+// Use the prompt function to get user input and store it in a variable
+let userInput = prompt("Please enter something:");
+
+// Check if the user entered something
+if (userInput !== null) {
+  // Display the user's input using console.log
+  console.log("You entered: " + userInput);
+} else {
+  // Handle the case where the user cancels the prompt
+  console.log("User cancelled the prompt.");
+}
+
+/*
+This is a multi-line comment.
+It won't be executed as code.
+*/
+
+// This is a single-line comment.
+
+console.log("Aslamualikum !"); // This is also a single-line comment.
+
+// Generate a random decimal between 0 and 1
+let randomDecimal = Math.random();
+console.log("Random Decimal:", randomDecimal);
+
